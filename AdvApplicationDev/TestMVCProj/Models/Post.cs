def28701dd27d076a99edbc323ebf90a8cc45069
Sidharthgoenka1 <114124439+Sidharthgoenka1@users.noreply.github.com>
2023-10-03 19:36:07 -0700

    library management db connection

--- AdvApplicationDev/TestMVCProj/Models/Post.cs
+++ AdvApplicationDev/TestMVCProj/Models/Post.cs
@@ -8,7 +8,6 @@ namespace TestMVCProj.Models
         [Key]
         public int Id { get; set; }
         
-        [Required]
         public string Title { get; set; }
         public string? Content { get; set; }
         public DateTime CreatedAt { get; set; }
