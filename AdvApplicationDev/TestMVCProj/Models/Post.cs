616bc91d2ccaec7ec1924886a0b0f1d9ea2dffb5
Sidharthgoenka1 <114124439+Sidharthgoenka1@users.noreply.github.com>
2023-10-03 19:53:29 -0700

    Adding Post page to library management application

--- AdvApplicationDev/TestMVCProj/Models/Post.cs
+++ AdvApplicationDev/TestMVCProj/Models/Post.cs
@@ -8,6 +8,7 @@ namespace TestMVCProj.Models
         [Key]
         public int Id { get; set; }
         
+        [Required]
         public string Title { get; set; }
         public string? Content { get; set; }
         public DateTime CreatedAt { get; set; }
