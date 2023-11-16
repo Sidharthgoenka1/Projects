4c8ca63d760ef9344991b271222e1b6ee2ecba77
Sidharthgoenka1 <114124439+Sidharthgoenka1@users.noreply.github.com>
2023-11-16 14:51:40 -0800

    Google authentication test project

--- AdvApplicationDev/BlogApp/Data/AppDbContext.cs
+++ AdvApplicationDev/BlogApp/Data/AppDbContext.cs
@@ -5,18 +5,18 @@ namespace BlogApp
 public class AppDbContext : DbContext
     {
         public DbSet<Models.Post> Posts { get; set; }
-        private readonly IConfiguration _configuration;
+        // private readonly IConfiguration _configuration;
 
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options){
             
         }
-        public AppDbContext(IConfiguration configuration)
-        {
-            _configuration = configuration;
-        }
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            optionsBuilder.UseSqlite(_configuration.GetConnectionString("DefaultConnection"));
-        }
+        // public AppDbContext(IConfiguration configuration)
+        // {
+        //     _configuration = configuration;
+        // }
+        // protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        // {
+        //     optionsBuilder.UseSqlite(_configuration.GetConnectionString("DefaultConnection"));
+        // }
     }
 }
--- AdvApplicationDev/BlogApp/obj/Debug/net6.0/BlogApp.AssemblyInfo.cs
+++ AdvApplicationDev/BlogApp/obj/Debug/net6.0/BlogApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlogApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+64276702eb6c91b0d0c7889ccf1c945e3fe70dfe")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlogApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlogApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
