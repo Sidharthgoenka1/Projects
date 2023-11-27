7a06fba4bb41696a2b491f396daafd60e23f4dab
Sidharthgoenka1 <114124439+Sidharthgoenka1@users.noreply.github.com>
2023-11-26 18:22:30 -0800

    Adding exception pages and records to DB

--- AdvApplicationDev/LibraryManagement_2/LibraryManagementProject/Controllers/HomeController.cs
+++ AdvApplicationDev/LibraryManagement_2/LibraryManagementProject/Controllers/HomeController.cs
@@ -24,8 +24,17 @@ public class HomeController : Controller
     }
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    public IActionResult Error()
+    public IActionResult Error(int? statusCode = null)
     {
+        if (statusCode.HasValue)
+        {
+            if (statusCode == 404 || statusCode == 500)
+            {
+                var viewName = statusCode.ToString();
+                return View(viewName);
+            }
+        }
+
         return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
     }
 }
--- AdvApplicationDev/LibraryManagement_2/LibraryManagementProject/Program.cs
+++ AdvApplicationDev/LibraryManagement_2/LibraryManagementProject/Program.cs
@@ -49,6 +49,8 @@ else
     app.UseHsts();
 }
 
+app.UseStatusCodePagesWithReExecute("/Home/Error", "?statusCode={0}");
+
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
