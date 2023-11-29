d91d5c243cd80eb2c4e8a52407a97c5fd80ac96b
Sidharthgoenka1 <114124439+Sidharthgoenka1@users.noreply.github.com>
2023-11-28 18:16:32 -0800

    URI Project in dotnet example

---
+++ AdvApplicationDev/UriExample/UriExample/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+ 
+class Program
+{
+    static void Main()
+    {
+        const string uriString = "https://www.fdu.edu/campuses/vancouver-campus/about-vancouver-campus/this-month/campus-updates/#ugtuition";
+        Uri canonicalUri = new(uriString);
+ 
+        Console.WriteLine(canonicalUri.Host);
+        Console.WriteLine(canonicalUri.PathAndQuery);
+        Console.WriteLine(canonicalUri.Fragment);
+    }
+}
